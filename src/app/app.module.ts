5016cd7933279be8a1b370a16367a3bebc0dd0de
problemsolver72221 <problemsolver72221@gmail.com>
2023-01-06 21:56:09 +0100

    course project - dynamic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 
 @NgModule({
@@ -38,7 +40,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -56,6 +60,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
       multi: true // set this to "true" even you are using one
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule { }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
+
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     isLoginMode = true
     isLoading = false
     error: string = null
+    // 'PlaceholderDirective' finds the first occurence in the DOM
+    @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective
 
-    constructor(private authService: AuthService, private router: Router) { }
+    // dynamic component subscription ref:
+    private closeSub: Subscription
+
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private componentFactoryResolver: ComponentFactoryResolver
+    ) { }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode
@@ -48,10 +60,49 @@ export class AuthComponent {
             errorMessage => {
                 console.log('ERR', errorMessage)
                 this.error = errorMessage
+                // set the error alert comp programmatically
+                this.showErrorAlert(errorMessage)
                 this.isLoading = false
             })
 
 
         form.reset()
+    }
+
+    onHandleError() {
+        this.error = null
+    }
+
+    private showErrorAlert(message: string) {
+        // this returns a component factory - so it is something just knows how to create components:
+        const alertCompFactory = this.componentFactoryResolver.resolveComponentFactory(
+            AlertComponent
+        )
+
+        // get the component to be attached via defined PlaceholderDirective reference (alertHost):
+        const hostViewContainerRef = this.alertHost.viewContainerRef
+        
+        // clear anything that might have been rendered there before,
+        // clear before you render anything something new:
+        hostViewContainerRef.clear()
+
+        // createComponent takes the component factory as argument:
+        const componentRef = hostViewContainerRef.createComponent(alertCompFactory)
+
+        // Pass props & events
+        componentRef.instance.message = message
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            // clear it here - because the component will be removed with the close method
+            this.closeSub.unsubscribe()
+
+            // clear the all content rendered on the parent comp:
+            hostViewContainerRef.clear()
+        })
+    }
+
+    ngOnDestroy() {
+        if (this.closeSub) {
+            this.closeSub.unsubscribe()
+        }
     }
 }
